224cbc816eef0b091ea50975a0aa5653f77151c8
Aditya Vyavahare <vvaditya23@icloud.com>
2023-10-01 19:11:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,29 @@
-#Step 1 
 import random
 
 word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+#Create an empty List called display.
+#For each letter in the chosen_word, add a "_" to 'display'.
+#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
 chosen_word = random.choice(word_list)
 print(chosen_word)
+display = []
+chosen_count = len(chosen_word)
+for _ in range(chosen_count):
+  display.append("_")
+#print(display)
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 guess = input("Guess the letter: ").lower()
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-for alphabet in chosen_word:
-  if alphabet == guess:
-    print("Right!")
-  else:
-    print("Wrong!")
-
-#step 2
-
-#TODO-1: - Create an empty List called display.
-#For each letter in the chosen_word, add a "_" to 'display'.
-#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
-
-#TODO-2: - Loop through each position in the chosen_word;
+#Loop through each position in the chosen_word;
 #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-
-#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
+for index, alphabet in enumerate(chosen_word):
+  if alphabet == guess:
+    display[index] = guess
+    print(display)
+  else:
+    print("Wrong!")
