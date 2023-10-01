c055c3b01124e3846ccb48ba249014528e946006
Aditya Vyavahare <vvaditya23@icloud.com>
2023-10-01 20:57:35 +0530

    configured the wrong input condition.

--- main.py
+++ main.py
@@ -2,6 +2,62 @@ import random
 
 word_list = ["aardvark", "baboon", "aabb"]
 
+stages = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''',  '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''']
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 #Create an empty List called display.
 #For each letter in the chosen_word, add a "_" to 'display'.
@@ -11,14 +67,19 @@ print(chosen_word)
 display = []
 won = False
 guess = ""
+lives = 0
+player_won = False
 
 chosen_count = len(chosen_word)
 for _ in range(chosen_count):
   display.append("_")
-print(display)
+  #Join all the elements in the list and turn it into a String.
+print(f"{' '.join(display)}")
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-while "_" in display:
+#while "_" in display:
+while not player_won:
+  
   guess = input("Guess the letter: ").lower()
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
@@ -31,9 +92,15 @@ while "_" in display:
   for index, alphabet in enumerate(chosen_word):
     if alphabet == guess:
       display[index] = guess
-    else:
-      print("Wrong!")
-    
-  print(display)
 
-print("You won!")
+  if guess not in chosen_word:
+    lives += 1
+    print(stages[lives - 1])
+    if lives == 7:
+      print("You loose!")
+      break
+  print(f"{' '.join(display)}")
+
+  if not "_" in display:
+    player_won = True
+    print("You won!")
