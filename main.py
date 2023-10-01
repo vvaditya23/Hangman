f0044f231479a86eb6896b130cf8cce340dfbe31
Aditya Vyavahare <vvaditya23@icloud.com>
2023-10-01 19:44:06 +0530

    Completed the winning situation.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 
-word_list = ["aardvark", "baboon", "camel"]
+word_list = ["aardvark", "baboon", "aabb"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 #Create an empty List called display.
@@ -9,21 +9,31 @@ word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 print(chosen_word)
 display = []
+won = False
+guess = ""
+
 chosen_count = len(chosen_word)
 for _ in range(chosen_count):
   display.append("_")
-#print(display)
+print(display)
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Guess the letter: ").lower()
+while "_" in display:
+  guess = input("Guess the letter: ").lower()
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 #Loop through each position in the chosen_word;
 #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-for index, alphabet in enumerate(chosen_word):
-  if alphabet == guess:
-    display[index] = guess
-    print(display)
-  else:
-    print("Wrong!")
+
+#Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+
+  for index, alphabet in enumerate(chosen_word):
+    if alphabet == guess:
+      display[index] = guess
+    else:
+      print("Wrong!")
+    
+  print(display)
+
+print("You won!")
